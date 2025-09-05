ced47feb5fd7a559fbb8f3cbdaf041d49f47afcb
FOX DONFACK PASCAL ARTHUR MONTGOMERY <118616410+Tiger-Foxx@users.noreply.github.com>
2025-09-05 17:50:09 +0100

    Create install_suricata.sh

---
+++ install_suricata.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+export DEBIAN_FRONTEND=noninteractive
+
+# update
+apt update && apt upgrade -y
+
+# prerequisites for Suricata (PPA)
+apt install -y software-properties-common curl jq
+add-apt-repository ppa:oisf/suricata-stable -y
+apt update
+apt install -y suricata iptables-persistent
+
+# enable ipv4 forwarding
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf || echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
+# add iptables rule to send FORWARD tcp:80 to NFQUEUE 0
+iptables -I FORWARD -p tcp --dport 80 -j NFQUEUE --queue-num 0
+netfilter-persistent save
+
+echo "Suricata install done" > /root/suricata_install_done.txt
