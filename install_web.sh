848fe22fd64f843b98a7a6ac69f48b05881e4238
FOX DONFACK PASCAL ARTHUR MONTGOMERY <118616410+Tiger-Foxx@users.noreply.github.com>
2025-09-05 18:26:23 +0100

    Update install_web.sh

--- install_web.sh
+++ install_web.sh
@@ -4,3 +4,5 @@ apt update && apt install -y nginx
 echo "<h1>Test Server $(hostname)</h1>" > /var/www/html/index.html
 systemctl restart nginx
 echo "Web install done" > /root/web_install_done.txt
+sudo ip addr add 10.0.2.2/24 dev eth0
+sudo ip route add default via 10.0.2.1
