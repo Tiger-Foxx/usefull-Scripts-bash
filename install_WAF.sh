024f7fd7b77e3b838205f1b3bdef032edd8f4e85
FOX DONFACK PASCAL ARTHUR MONTGOMERY <118616410+Tiger-Foxx@users.noreply.github.com>
2025-09-05 18:36:21 +0100

    Update install_WAF.sh

--- install_WAF.sh
+++ install_WAF.sh
@@ -3,3 +3,4 @@ sudo ip addr add 10.0.2.1/24 dev eth1  # lien vers Webserver
 sudo ip link set eth0 up; sudo ip link set eth1 up
 # config route par défaut pour reachability:
 sudo ip route add default via 10.0.1.1
+sudo apt install tcpdump
